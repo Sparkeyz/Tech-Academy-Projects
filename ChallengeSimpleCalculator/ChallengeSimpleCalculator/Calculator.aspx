1f593c681b94dcebcc3824b06afdc80ade2305c1
Austin Chang <austinheneh@gmail.com>
2017-08-07 17:17:35 -0700

    Added a simple calculator implementation
    Added a simple calculator implementation drill in C# and Visual Studio.

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Calculator.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="ChallengeSimpleCalculator.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+            <p>
+                First Value:&nbsp;
+                <asp:TextBox ID="ValueBox1" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                Second Value:&nbsp;
+                <asp:TextBox ID="ValueBox2" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="Plus" runat="server" OnClick="Button1_Click" Text=" + " />
+&nbsp;<asp:Button ID="Minus" runat="server" Text=" - " OnClick="Minus_Click" />
+&nbsp;<asp:Button ID="Multiply" runat="server" Text=" * " OnClick="Multiply_Click" />
+&nbsp;<asp:Button ID="Divide" runat="server" Text=" / " OnClick="Divide_Click" />
+&nbsp;</p>
+            <p>
+                <asp:Label ID="Result" runat="server"></asp:Label>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Calculator.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double val1 = double.Parse(ValueBox1.Text);
+            double val2 = double.Parse(ValueBox2.Text);
+
+            double result = val1 + val2;
+            Result.Text = result.ToString();
+            /*Note that these four methods are repetitive and could be
+              easily optimized, but the tutorial didn't get that far yet
+              so I left it as is*/
+        }
+
+        protected void Minus_Click(object sender, EventArgs e)
+        {
+            double val1 = double.Parse(ValueBox1.Text);
+            double val2 = double.Parse(ValueBox2.Text);
+
+            double result = val1 - val2;
+            Result.Text = result.ToString();
+        }
+
+        protected void Multiply_Click(object sender, EventArgs e)
+        {
+            double val1 = double.Parse(ValueBox1.Text);
+            double val2 = double.Parse(ValueBox2.Text);
+
+            double result = val1 * val2;
+            Result.Text = result.ToString();
+        }
+
+        protected void Divide_Click(object sender, EventArgs e)
+        {
+            double val1 = double.Parse(ValueBox1.Text);
+            double val2 = double.Parse(ValueBox2.Text);
+
+            double result = val1 / val2;
+            Result.Text = result.ToString();
+        }
+    }
+}
