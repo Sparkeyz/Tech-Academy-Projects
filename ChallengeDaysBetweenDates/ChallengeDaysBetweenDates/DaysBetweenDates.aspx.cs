318e38ecf4899877d161c2bd0b5eaac8e358bef1
Austin Chang <austinheneh@gmail.com>
2017-08-10 16:43:55 -0700

    Added Days Between Dates
    Days Between Dates provides two calendars from which you can select dates. When the OK button is pressed, the application will determine the number of days between the two selected dates and print it out on screen.

---
+++ ChallengeDaysBetweenDates/ChallengeDaysBetweenDates/DaysBetweenDates.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class DaysBetweenDates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Calendar1.SelectedDate > Calendar2.SelectedDate)
+            {
+                ElapsedDays.Text = Calendar1.SelectedDate
+                    .Subtract(Calendar2.SelectedDate)
+                    .TotalDays.ToString();
+            } else
+            {
+                ElapsedDays.Text = Calendar2.SelectedDate
+                    .Subtract(Calendar1.SelectedDate)
+                    .TotalDays.ToString();
+            }
+        }
+    }
+}
