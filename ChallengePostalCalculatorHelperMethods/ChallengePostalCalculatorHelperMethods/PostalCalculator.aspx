0731d5f59268025b5ef89c3c1bc536ea6ab40c29
Austin Chang <austinheneh@gmail.com>
2017-08-12 20:37:41 -0700

    Added PostalCalculator Challenge
    A coding drill based on the creation and usage of helper methods.

---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/PostalCalculator.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PostalCalculator.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.PostalCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator<br />
+            <br />
+            Width:
+            <asp:TextBox ID="WidthBox" runat="server" AutoPostBack="True" OnTextChanged="WidthBox_TextChanged"></asp:TextBox>
+            <br />
+            Height: <asp:TextBox ID="HeightBox" runat="server" AutoPostBack="True" OnTextChanged="HeightBox_TextChanged"></asp:TextBox>
+            <br />
+            Length:
+            <asp:TextBox ID="LengthBox" runat="server" AutoPostBack="True" OnTextChanged="LengthBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="GroundButton" runat="server" GroupName="Shipping" Text="Ground" AutoPostBack="True" OnCheckedChanged="GroundButton_CheckedChanged" />
+            <br />
+            <asp:RadioButton ID="AirButton" runat="server" GroupName="Shipping" Text="Air" AutoPostBack="True" OnCheckedChanged="AirButton_CheckedChanged" />
+            <br />
+            <asp:RadioButton ID="NextDayButton" runat="server" GroupName="Shipping" Text="Next Day" AutoPostBack="True" OnCheckedChanged="NextDayButton_CheckedChanged" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/PostalCalculator.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class PostalCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        protected void GroundButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        protected void AirButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        protected void NextDayButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        protected void WidthBox_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        protected void HeightBox_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        protected void LengthBox_TextChanged(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void calculate()
+        {
+            if (WidthBox.Text.Trim().Length!=0 && HeightBox.Text.Trim().Length!=0)
+            {
+                int width = int.Parse(WidthBox.Text);
+                int height = int.Parse(HeightBox.Text);
+                if (AirButton.Checked)
+                {
+                    findPrice(0.25, width, height);
+                } else if (GroundButton.Checked)
+                {
+                    findPrice(0.15, width, height);
+                } else if (NextDayButton.Checked)
+                {
+                    findPrice(0.45, width, height);
+                } else
+                {
+                    return;
+                }
+            } else
+            {
+                return;
+            }
+        }
+
+        private void findPrice(double multiplier, int width, int height)
+        {
+            int length = 1;
+            if (LengthBox.Text.Trim().Length!=0)
+            {
+                length = int.Parse(LengthBox.Text.Trim().ToString());
+            }
+            double price = multiplier * width * length * height;
+            resultLabel.Text = string.Format("Your parcel will cost ${0:C}", price.ToString());
+        }
+    }
+}
